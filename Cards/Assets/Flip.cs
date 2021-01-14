83a402cfcd905a3dda674a034086a0870ef99bb3
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-13 19:22:14 -0500

    new flip card changes

--- Cards/Assets/Flip.cs
+++ Cards/Assets/Flip.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CardOne : MonoBehaviour
+public class Flip : MonoBehaviour
 {
+    
     public float x, y, z;
     public GameObject CardBack;
     public bool cardBackisActive;
     public int timer;
-    
+   
 
     // Start is called before the first frame update
     void Start()
@@ -24,48 +24,54 @@ public class CardOne : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-           
+
             Startflip();
         }
     }
 
-   public void Startflip ()
+
+    public void Startflip()
 
     {
         StartCoroutine(CalculatedFlip());
 
     }
 
-    public void Flip()
+    public void flip()
 
     {
+
         if (cardBackisActive == true)
         {
-            CardBack.SetActive(false); // false
-            cardBackisActive = false; // false
+            CardBack.SetActive(false); 
+            cardBackisActive = false; 
+           
         }
 
 
         else
         {
-            CardBack.SetActive(true);// true
-            cardBackisActive = true; // true
+            CardBack.SetActive(true);
+            cardBackisActive = true; 
         }
+
+
+
 
     }
 
-     IEnumerator CalculatedFlip()
+    IEnumerator CalculatedFlip()
 
     {
-        for (int i =0; i < 180; i++)
+        for (int i = 0; i < 180; i++)
         {
-            yield return new WaitForSeconds(0.06f);
-            transform.Rotate(new Vector3(x, y, z));
+            yield return new WaitForSeconds(0.02f);
+            transform.Rotate(new Vector3(x, y)); //x y z
             timer++;
 
-            if(timer == 90 || timer == - 90)
+            if (timer == 60 || timer == -60)
             {
-                Flip();
+                flip();
             }
 
         }
@@ -73,6 +79,4 @@ public class CardOne : MonoBehaviour
         timer = 0;
 
     }
-
-
-   }
+}
